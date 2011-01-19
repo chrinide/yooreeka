fa3b4e629b3b6b7fb1931636057b5ea8263e6326
babis.marmanis <babis.marmanis@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2011-01-19 19:37:27 +0000

    Added two baseline methods that compare the RMSE of the kNN prediction with a prediction that is based on mere averaging either on Item or on User

--- src/iweb2/ch3/collaborative/evaluation/RMSEEstimator.java
+++ src/iweb2/ch3/collaborative/evaluation/RMSEEstimator.java
@@ -85,6 +85,65 @@ public class RMSEEstimator {
         
     }
     
+    public void compareRMSEs(Recommender delphi) {
+        
+        MovieLensDataset ds = (MovieLensDataset)delphi.getDataset(); 
+        Collection<Rating> testRatings = ds.getTestRatings();
+        
+        compareRMSEs(delphi, testRatings);
+    }
+
+    
+    public void compareRMSEs(Recommender delphi, Collection<Rating> testRatings) {
+    
+        double sum = 0.0;
+        double sumAvgItem = 0.0;
+        double sumAvgUser = 0.0;
+        
+        Dataset ds = delphi.getDataset();
+
+        int totalSamples = testRatings.size();
+        
+        if( verbose ) {
+            System.out.println("Calculating RMSE ...");
+            System.out.println("Training ratings count: " + ds.getRatingsCount());
+            System.out.println("Test ratings count: " + testRatings.size());
+        }
+        
+        for(Rating r : testRatings ) {
+            User user = ds.getUser(r.getUserId());
+            Item item = ds.getItem(r.getItemId());
+            double predictedItemRating = delphi.predictRating(user, item);
+            double predictedAvgItemRating = delphi.predictBasedOnItemAverage(item);
+            double predictedAvgUserRating = delphi.predictBasedOnUserAverage(user);
+            
+            if( predictedItemRating > 5.0 ) {
+                predictedItemRating = 5.0;
+                //System.out.println("Predicted item rating: " + predictedItemRating);
+            }
+//            if( verbose ) {
+//                System.out.println(
+//                        "user: " + r.getUserId() + 
+//                        ", item: " + r.getItemId() +
+//                        ", actual rating: " + r.getRating() + 
+//                        ", predicted: " + String.valueOf(predictedItemRating));
+//            }
+
+            sum += Math.pow((predictedItemRating - r.getRating()), 2);
+            sumAvgItem += Math.pow((predictedAvgItemRating - r.getRating()), 2);
+            sumAvgUser += Math.pow((predictedAvgUserRating - r.getRating()), 2);
+            
+        }
+        
+        double rmse = Math.sqrt(sum / totalSamples); 
+        double rmseAvgItem = Math.sqrt(sumAvgItem / totalSamples); 
+        double rmseAvgUser = Math.sqrt(sumAvgUser / totalSamples); 
+
+        System.out.println("RMSE:" + rmse);
+        System.out.println("RMSE (based on avg. Item rating):" + rmseAvgItem);
+        System.out.println("RMSE (based on avg. User rating):" + rmseAvgUser);
+    }
+    
     public boolean isVerbose() {
         return verbose;
     }
