9285467f3ae41e67ca9a75e8caf9b15a71782b11
aristotle.of.stageira@gmail.com <aristotle.of.stageira@gmail.com@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2012-08-29 17:33:01 +0000

    The credit example should stand by itself since it is an independent case

---
+++ trunk/src/org/yooreeka/examples/credit/BaggingCreditClassifier.java
@@ -0,0 +1,49 @@
+package org.yooreeka.examples.credit;
+
+import org.yooreeka.algos.taxis.core.TrainingSet;
+import org.yooreeka.algos.taxis.core.intf.Concept;
+import org.yooreeka.algos.taxis.ensemble.ClassifierEnsemble;
+import org.yooreeka.examples.credit.data.UserDataset;
+import org.yooreeka.examples.credit.data.users.User;
+import org.yooreeka.examples.credit.util.BootstrapTrainingSetBuilder;
+import org.yooreeka.examples.credit.util.UserInstanceBuilder;
+
+
+public class BaggingCreditClassifier extends ClassifierEnsemble {
+
+    private UserInstanceBuilder instanceBuilder;
+    private BootstrapTrainingSetBuilder bootstrapTSetBuilder;
+    
+    public BaggingCreditClassifier(UserDataset ds) {
+
+        super(BaggingCreditClassifier.class.getSimpleName());
+        
+        /* Creating instance builder for this classifier */
+        instanceBuilder = new UserInstanceBuilder(false);
+        
+
+        /* Creating original training set that will be used to generate 
+         * bootstrap sets */
+        TrainingSet originalTSet = instanceBuilder.createTrainingSet(ds); 
+        
+        bootstrapTSetBuilder = new BootstrapTrainingSetBuilder(originalTSet);          
+    }
+    
+    public TrainingSet getBootstrapSet() {
+    	return bootstrapTSetBuilder.buildBootstrapSet();
+    }
+        
+    public UserInstanceBuilder getInstanceBuilder() {
+        return instanceBuilder;
+    }
+    
+    public Concept classify(User user) {
+
+        if (verbose) {
+            System.out.println("User:\n  >> "+user.toString());
+        }
+        
+        return classify(instanceBuilder.createInstance(user));
+    }
+    
+}
