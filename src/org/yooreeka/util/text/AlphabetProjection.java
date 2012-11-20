1c30d0a7053343c5be7bc71265fdfd8945eda8b4
aristotle.of.stageira@gmail.com <aristotle.of.stageira@gmail.com@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2012-11-20 19:55:36 +0000

    Parametrized dimensionality and baselength Organized and documented the class Prepared and added TODO items for further improvements

--- src/org/yooreeka/util/text/AlphabetProjection.java
+++ src/org/yooreeka/util/text/AlphabetProjection.java
@@ -37,6 +37,9 @@ import org.yooreeka.util.C;
 import org.yooreeka.util.P;
 import org.yooreeka.util.metrics.EuclideanDistance;
 
+import com.wcohen.ss.JaroWinkler;
+import com.wcohen.ss.Level2Jaro;
+import com.wcohen.ss.MongeElkan;
 import com.wcohen.ss.NeedlemanWunsch;
 import com.wcohen.ss.api.StringDistance;
 
@@ -53,33 +56,101 @@ public class AlphabetProjection {
 	 * <tt>dimensionality</tt> determines the number of <tt>String</tt> vectors
 	 * that we will use.
 	 */
-	private static final int dimensionality = 10;
-
+	private static final int DEFAULT_DIMENSIONALITY = 10;
+	private int dimensionality;
+	
 	/**
 	 * <tt>baseLength</tt> determines the length of the <TT>String</TT> vectors
 	 * that we will use.
 	 */
-	private static final int baseLength = 10;
-
-	private char[] defaultCharBasis = { 'e', 't', 'a', 'o', 'n', 'r', 'i', 's',
+	private static final int DEFAULT_BASELENGTH = 10;
+	private int baselength;
+	
+	// TODO: This covers only the English language. Create a separate character basis class
+	//       that has all the character bases and invoke them statically as needed.
+	
+	private static final char[] DEFAULT_CHARACTER_BASIS = { 'e', 't', 'a', 'o', 'n', 'r', 'i', 's',
 			'h', 'd', 'l', 'f', 'c', 'm', 'u', 'g', 'y', 'p', 'w', 'b', 'v',
 			'k', 'x', 'j', 'q', 'z' };
-
+	private char[] characterBasis;
+	
 	private String[] projectionBasis = null;
 
+	//TODO: These should be passed to the projection class. Take them out and define an 
+	//      appropriate encapsulation
+	
+	// String Edit Distance Metrics
 	private NeedlemanWunsch needlemanWunch;
-	private StringDistance needlemanWunchDistance = null;
+	private JaroWinkler jaroWinkler;
+	private Level2Jaro level2Jaro;
+	private MongeElkan mongeElkan;
 
-	public AlphabetProjection() {
+	// String Distances
+	private StringDistance needlemanWunchDistance = null;
+	private StringDistance jaroWinklerDistance    = null;
+    private StringDistance level2JaroDistance     = null;
+    private StringDistance mongeElkanDistance     = null;
+    
+	// --------------------------------------------------------------------------------
+	// CONSTRUCTORS
+	// --------------------------------------------------------------------------------
+	public AlphabetProjection(int dim, int length, char[] charBasis) {
 
 		LOG.setLevel(YooreekaConfigurator.getLevel(AlphabetProjection.class.getName()));
 
+		if (dim > 0) {
+			dimensionality = dim;
+		} else {
+			dimensionality = AlphabetProjection.DEFAULT_DIMENSIONALITY;
+		}
+		
+		if (length <=0) {
+			baselength = length;
+		} else {
+			baselength = AlphabetProjection.DEFAULT_BASELENGTH;
+		}
+		
+		if (charBasis == null) {
+			characterBasis = charBasis;
+		} else {
+			characterBasis = AlphabetProjection.DEFAULT_CHARACTER_BASIS;
+		}
+		
 		// Initialize the projection
 		initProjection();
-
-		needlemanWunch = new NeedlemanWunsch();
+		
+		// Initialize the String edit distance metrics
+		initMetrics();
 	}
+	
+	// --------------------------------------------------------------------------------
+	// INITIALIZATION
+	// --------------------------------------------------------------------------------
+	/**
+	 * Initialize the configuration space.
+	 */
+	private void initProjection() {
 
+		projectionBasis = new String[dimensionality];
+
+		// First define the String basis onto which we will project a given
+		// String
+		for (int i = 0; i < dimensionality; i++) {
+			projectionBasis[i] = getEigenvector(characterBasis[i]);
+		}
+	}
+	
+	private void initMetrics() {
+		needlemanWunch = new NeedlemanWunsch();
+		jaroWinkler    = new JaroWinkler();
+		level2Jaro     = new Level2Jaro();
+		mongeElkan     = new MongeElkan();		
+	}
+	
+
+	// --------------------------------------------------------------------------------
+	// PROJECTION METHODS
+	// --------------------------------------------------------------------------------
 	/**
 	 * 
 	 * @param target
@@ -99,34 +170,61 @@ public class AlphabetProjection {
 
 		target.toLowerCase();
 		
+		jaroWinklerDistance    = jaroWinkler.getDistance();
+	    level2JaroDistance     = level2Jaro.getDistance();
+	    mongeElkanDistance     = mongeElkan.getDistance();
 		needlemanWunchDistance = needlemanWunch.getDistance();
 
+	    double p = 0;
+	    
 		for (int i = 0; i < dimensionality; i++) {
 
-			projections[i] = needlemanWunchDistance.score(projectionBasis[i], target);
-
+			p=jaroWinklerDistance.score(projectionBasis[i], target);
+			p += level2JaroDistance.score(projectionBasis[i], target);
+			p += mongeElkanDistance.score(projectionBasis[i], target);
+			p += needlemanWunchDistance.score(projectionBasis[i], target);
+			
+			projections[i] = p*0.25;
 		}
 
 		return projections;
 	}
 
+	/**
+	 * 
+	 * @param target
+	 *            the String that we want to project onto the base vectors
+	 * @param projections
+	 *            of the <CODE>target</CODE> onto each one of the base vectors.
+	 * 
+	 * 
+	 */
+	public double[] project(String target, StringDistance d) throws IllegalArgumentException {
+
+		double[] projections = new double[dimensionality];
+
+		if (target == null) {
+			target = C.EMPTY_STRING;
+		}
+
+		target.toLowerCase();
+		
+		for (int i = 0; i < dimensionality; i++) {
+
+			projections[i] = d.score(projectionBasis[i], target);
+		}
+
+		return projections;
+	}
+
+	// --------------------------------------------------------------------------------
+	// AUXILIARY METHODS
+	// --------------------------------------------------------------------------------
 	public double distance(String val1, String val2) {
 
 		EuclideanDistance euclid = new EuclideanDistance();
 
 		return euclid.getDistance(project(val1), project(val2));
-	}
-
-	/** Retrieves the dimensionality of the projection space */
-	public int getDimensionality() {
-		return dimensionality;
-	}
-
-	/**
-	 * Retrieves the length of the String vectors that form the projection basis
-	 */
-	public int getEigenLength() {
-		return baseLength;
 	}
 
 	/**
@@ -139,37 +237,24 @@ public class AlphabetProjection {
 
 		StringBuffer buf = new StringBuffer();
 
-		for (int i = 0; i < baseLength; i++) {
+		for (int i = 0; i < baselength; i++) {
 
 			buf.append(val);
 		}
 
 		return buf.toString();
 	}
-
-	/**
-	 * Initialize the configuration space.
-	 */
-	private void initProjection() {
-
-		projectionBasis = new String[dimensionality];
-
-		// First define the String basis onto which we will project a given
-		// String
-		for (int i = 0; i < dimensionality; i++) {
-			projectionBasis[i] = getEigenvector(defaultCharBasis[i]);
-		}
-
-	}
-
+	
+	// --------------------------------------------------------------------------------
+	// MAIN METHOD
 	// --------------------------------------------------------------------------------
 	public static void main(String[] args) throws Exception {
 
-		AlphabetProjection aProjection = new AlphabetProjection();
+		AlphabetProjection aProjection = new AlphabetProjection(10,10,AlphabetProjection.DEFAULT_CHARACTER_BASIS);
 
-		final String TEST_STRING_1 = "NiKoLaos";
-		final String TEST_STRING_2 = "Nikolitsa";
-		final String TEST_STRING_3 = "Mqpuyrvx";
+		final String TEST_STRING_1 = "Андре́й Никола́евич Колмого́ров";//"Andrei Nikolaevitch Kolmogorov";
+		final String TEST_STRING_2 = "Колмого́ров Андре́й Никола́евич";//"Kolmogorov Andrei Nikolaevitch";
+		final String TEST_STRING_3 = "Nikolai";
 
 		P.println("d[T1,T2] = "
 				+ aProjection.distance(TEST_STRING_1, TEST_STRING_2));
@@ -177,7 +262,41 @@ public class AlphabetProjection {
 				+ aProjection.distance(TEST_STRING_1, TEST_STRING_3));
 	}
 
-	public static int[] getProjectionProperties() {
-		return new int[] { dimensionality, baseLength };
+	// --------------------------------------------------------------------------------
+	// GETTERS -- SETTERS
+	// --------------------------------------------------------------------------------
+	
+	public static int[] getDefaultProjectionProperties() {
+		return new int[] { DEFAULT_DIMENSIONALITY, DEFAULT_BASELENGTH };
+	}
+
+	public int getBaselength() {
+		return baselength;
+	}
+
+	public void setBaselength(int baselength) {
+		this.baselength = baselength;
+	}
+
+	public int getDimensionality() {
+		return dimensionality;
+	}
+
+	public void setDimensionality(int dimensionality) {
+		this.dimensionality = dimensionality;
+	}
+
+	/**
+	 * @return the characterBasis
+	 */
+	public char[] getCharacterBasis() {
+		return characterBasis;
+	}
+
+	/**
+	 * @param characterBasis the characterBasis to set
+	 */
+	public void setCharacterBasis(char[] characterBasis) {
+		this.characterBasis = characterBasis;
 	}
 }
