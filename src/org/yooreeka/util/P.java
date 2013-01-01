2533a937c88b3bf1f573fc818e88e6bb6d288d95
aristotle.of.stageira@gmail.com <aristotle.of.stageira@gmail.com@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2013-01-01 22:09:49 +0000

    Minor clean up

--- src/org/yooreeka/util/P.java
+++ src/org/yooreeka/util/P.java
@@ -40,12 +40,33 @@ import java.nio.charset.Charset;
 public class P {
 
 	/**
-	 * Print a 54 character (-) horizontal line.
+	 * Print a horizontal line with 65 characters.
 	 */
 	public static void hline() {
 		println("---------- ---------- ---------- ---------- ---------- ----------");
 	}
-
+	
+	
+	/**
+	 * Auxiliary method for sending time information to the standard output. 
+	 * Time is measured in milliseconds, see the documentation
+	 * of <tt>System.currentTimeMillis()</tt> for details.
+	 * 
+	 */
+	public static void time() {
+		println("Time: "+System.currentTimeMillis());
+	}
+	
+	/**
+	 * Auxiliary method for sending time information to the standard output.
+	 * The time is given in milliseconds and in relation to a given moment
+	 * in the past, determined by the value of the argument <tt>t</tt>. 
+	 * 
+	 */
+	public static void time(long t) {
+		println("Time: "+ (System.currentTimeMillis()-t));
+	}
+	
 	public static void main(String[] args) {
 		println(Charset.defaultCharset().displayName());
 		println("" + P.class.getName());
