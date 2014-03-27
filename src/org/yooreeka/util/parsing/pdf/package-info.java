8171cce85a28f679716a1e54613fd7bfa938b1be
aristotle.of.stageira@gmail.com <aristotle.of.stageira@gmail.com@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2014-03-27 01:22:24 +0000

    Update

--- src/org/yooreeka/util/parsing/pdf/package-info.java
+++ src/org/yooreeka/util/parsing/pdf/package-info.java
@@ -9,8 +9,8 @@
  *   (Manning 2009). Although the term "Web" prevailed in the title, in essence, the algorithms 
  *   are valuable in any software application.
  *  
- *   Copyright (c) 2007-2009 Haralambos Marmanis & Dmitry Babenko
- *   Copyright (c) 2009-${year} Marmanis Group LLC and individual contributors as indicated by the @author tags.  
+ *   Copyright (c) 2007-2009    Haralambos Marmanis & Dmitry Babenko
+ *   Copyright (c) 2009-2013 Marmanis Group LLC and individual contributors as indicated by the @author tags.  
  * 
  *   Certain library functions depend on other Open Source software libraries, which are covered 
  *   by different license agreements. See the NOTICE file distributed with this work for additional 
@@ -26,60 +26,10 @@
  *   the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
  *   either express or implied. See the License for the specific language governing permissions and
  *   limitations under the License.
- *   
+ *
  */
-package org.yooreeka.test;
-
-import org.yooreeka.math.Fibonacci;
-import org.yooreeka.util.P;
-import org.yooreeka.util.gui.XyGui;
-
-
-
 /**
- * 
  * @author <a href="mailto:babis@marmanis.com">Babis Marmanis</a>
  *
  */
-public class TestSandbox {
-
-	/**
-	 * Throw your code in the main method for quick tests.
-	 * This is useful when you are testing Beanshell scripts.
-	 * 
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		int seriesSize = 51;
-		double[] x = new double[seriesSize], y = new double[seriesSize];
-		
-		StringBuilder msg = new StringBuilder();
-		msg.append("Fib [").append(seriesSize-1).append("] = ");
-
-		Fibonacci[] fibonacci = new Fibonacci[seriesSize-2];
-		
-		for (int i=0; i<seriesSize; i++) {
-			for (int j=2; j<seriesSize; j++) {
-				fibonacci[j-2] = new Fibonacci(j,seriesSize);
-				
-				if (i == seriesSize-1) {
-					x[j-2] = (double) j;
-					y[j-2] = (double) fibonacci[j-2].get(i);
-					
-					if (j<seriesSize-1)
-						msg.append(fibonacci[j-2].get(i)).append(", ");
-					else
-						msg.append(fibonacci[j-2].get(i));
-				}
-			}
-		}
-		P.println(msg.toString());
-		P.hline();
-		P.println(x, y);
-		
-		XyGui g = new org.yooreeka.util.gui.XyGui ("A plot",x,y);
-		g.plot();
-
-	}
-}
+package org.yooreeka.util.parsing.pdf;
